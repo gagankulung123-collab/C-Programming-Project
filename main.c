f76a766b2ad8b9f83beb22ab393813c33584377a
Gagan Rai <gagankulung123@gmail.com>
2026-04-02 20:54:29 +0545

    Added main menu

--- main.c
+++ main.c
@@ -8,5 +8,13 @@ int main()
     printf(" LIFE COMPANION SYSTEM  \n");
     printf("========================\n");
     printf("Welcome!\n");
+
+    printf("\n--- MAIN MENU ---\n");
+    printf("1. Add Daily Tasks\n");
+    printf("2. View Tasks\n");
+    printf("3. Mood Check\n");
+    printf("4. Motivation Boost\n");
+    printf("5. Progress Report\n");
+    printf("6. Exit\n");
     return 0;
 }
