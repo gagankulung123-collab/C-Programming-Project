417ffb11eb5a3ebece4df31853c89a4cf3874ba1
Gagan Rai <gagankulung123@gmail.com>
2026-04-02 20:46:29 +0545

    First commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+int main()
+{
+    printf("========================\n");
+    printf(" LIFE COMPANION SYSTEM  \n");
+    printf("========================\n");
+    printf("Welcome!\n");
+    return 0;
+}
